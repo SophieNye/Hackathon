1251254b4ffddb0fb30a4731fefbeb9c245776e4
Sophie <sophie.nye@gmail.com>
2019-06-12 20:44:47 -0400

    functionality with recognizing URLS reached

--- background.js
+++ background.js
@@ -1,1 +1,28 @@
-console.log("background script up and running")
+// console.log("background script up and running")
+//create object
+const urls = ['tripadvisor.com', 'linkedin.com', 'facebook.com', 'spotify.com', 'amazon.com']
+
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        console.log('change info ', changeInfo)
+        alert('updated from background');
+        chrome.tabs.query({
+            active: true, // Select active tabs
+            lastFocusedWindow: true // In the current window
+        }, function (array_of_Tabs) {
+            // Since there can only be one active tab in one active window, 
+            //  the array has only one element
+            const tab = array_of_Tabs[0];
+            // Example:
+            const url = tab.url;
+            for (let i = 0; i < urls.length; i++) {
+                if (url.includes(urls[i])) {
+                    console.log(`${url} inclues ${urls[i]}: `, url.includes(urls[i]) ? true : false)
+                    return;
+                }
+            }
+            // ... do something with url variable
+        })
+    };
+});
