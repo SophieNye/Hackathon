a0999bb1f56952dee0d4fb09487a3da2f506b671
Ian Geckeler <ikcgeckeler@gmail.com>
2019-06-13 01:11:59 -0400

    all scripts talk to each other!

--- background.js
+++ background.js
@@ -1,31 +1,30 @@
 // console.log("background script up and running")
 //create object
-const urls = ['tripadvisor.com', 'linkedin.com', 'facebook.com', 'spotify.com', 'amazon.com']
-
+const urls = ['tripadvisor.com', 'linkedin.com', 'facebook.com', 'spotify.com', 'amazon.com'];
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status === 'complete') {
         console.log('change info ', changeInfo)
-        chrome.tabs.query({
-            active: true, // Select active tabs
-            lastFocusedWindow: true // In the current window
-        }, function (array_of_Tabs) {
-            // Since there can only be one active tab in one active window, 
-            //  the array has only one element
-            const tab = array_of_Tabs[0];
-            // Example:
             const url = tab.url;
+            console.log(url)
             for (let i = 0; i < urls.length; i++) {
                 if (url.includes(urls[i])) {
                     console.log(`${url} inclues ${urls[i]}: `, url.includes(urls[i]) ? true : false)
-                    injectContentScript();
+                    sendURLMessage(urls[i],tab);
                     return;
                 }
             }
             // ... do something with url variable
-        })
     };
 });
+
+function sendURLMessage(url,tab) {
+    console.log('tab:',tab)
+    chrome.tabs.sendMessage(tab.id, {type: "url",url:url}, function(response) {
+      console.log(response.farewell);
+    });
+}
+
 
 function injectContentScript() {
   chrome.tabs.executeScript({file: "contents_script.js"}, function() {
--- contents_script.js
+++ contents_script.js
@@ -1,21 +1,86 @@
-console.log("contents script up and running")
-
-const loginButton = document.querySelector("#login");
-const body = document.querySelector("body");
-let wrapper = document.querySelector("#modal-wrapper")
-
-body.style.backgroundColor = "rgba(0, 0, 0, 0.25)";
-wrapper.style.opacity = '1';
-wrapper.style.backgroundColor = 'lightgray';
-
-loginButton.animate([{
-        boxShadow: "0 0 8px 8px #fff"
-    },
-    {
-        boxShadow: "0 0 0 0 #fff"
+let selectorsObj = {
+  'tripadvisor.com': {
+  'search': '#component_10 > div > div > div > div > div > div > div.brand-header-GlobalNavActions__searchWrap--2XBZ1 > div',
+  'profile': '#component_10 > div > div > div > div > div > div > div:nth-child(6) > div > div:nth-child(1) > div > span > span',
+  'home':'',
+  'login':'.ui_icon.friend.brand-global-nav-action-profile-Profile__loggedOutIcon--YhUxY'
+  },
+  'linkedin.com': {
+    'search': '#ember33 > input[type=text]',
+    'profile': '#nav-settings__dropdown-trigger',
+    'home':'#feed-nav-item',
+    'login':'#component_12 > div > div > div > div > div > div > div:nth-child(6) > div > a.ui_button.primary.small.brand-global-nav-action-profile-Profile__loginButton--2fZJK'
     }
-], {
-    duration: 2000,
-    iterations: Infinity,
-    direction: 'alternate-reverse'
+}
+
+let selectors;
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log('request received not sure what type yet')
+    if (request.type == "url") {
+      console.log('request from background url received',request)
+      resetSelectors(request.url,selectorsObj)
+      sendResponse('request for',request.url,'received');
+    }
+    else if (request.type == "button") {
+      console.log('request from button received',request)
+      const selector = selectors[request.button];
+
+      addCSS(selector);
+
+      // const body = document.querySelector("body")
+      //   body.addEventListener("click", () => {
+      //     removeCSS(selector);
+      //   })
+
+    }
 });
+
+function resetSelectors(url, selectorsObj) {
+  selectors = selectorsObj[url];
+  console.log(selectors);
+}
+
+function addCSS(selector) {
+  console.log('add css: ', selector)
+  let button = document.querySelector(selector);
+  console.log('did I find button?',button)
+  let body = document.querySelector("body");
+  console.log(body)
+  body.style.backgroundColor = "rgba(0, 0, 0, 0.25)!important";
+  button.style.opacity = "1";
+  button.animate([{
+          boxShadow: "0 0 8px 8px #fff"
+      },
+      {
+          boxShadow: "0 0 0 0 #fff"
+      }
+  ], {
+      duration: 2000,
+      iterations: Infinity,
+      direction: 'alternate-reverse'
+  });
+}
+
+
+
+function removeCSS(selector) {
+  console.log('remove css: ', selector)
+  let button = document.querySelector(selector);
+  let body = document.querySelector("body");
+  body.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  button.style.opacity = "1";
+  button.animate([{
+          boxShadow: "0 0 0 0 #fff"
+      },
+      {
+          boxShadow: "0 0 0 0 #fff"
+      }
+  ], {
+      duration: 2000,
+      iterations: Infinity,
+      direction: 'alternate-reverse'
+  });
+}
+
